e4ca1db725ecf7fdf274a04fe98ea5190fe4cd39
Torben Carstens <github@carstens.tech>
2020-07-20 12:42:47 +0200

    Commit setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='telegram_bot',
+    version='0.1.0-alpha-dev',
+    packages=['telegram_bot'],
+    url='https://github.com/torben-carstens/telegram-template-bot',
+    license='MIT',
+    author='Torben Carstens',
+    author_email='telegramtemplatebot@carstens.tech',
+    description='Template for a telegram bot'
+)
