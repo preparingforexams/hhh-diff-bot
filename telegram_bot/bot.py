0f48b7f09229844a64cfa974a46d48a32738a829
Torben Carstens <github@carstens.tech>
2020-07-21 17:00:16 +0200

    Join groups with pipes
    Closes #9

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -142,7 +142,7 @@ class Bot:
         for _, g in groupby(
                 sorted([chat for _, chat in self.chats.items() if chat.title], key=lambda c: c.title.lower()),
                 key=lambda c: c.title[0].lower()):
-            text += ", ".join([chat.title for chat in g]) + "\n"
+            text += " | ".join([chat.title for chat in g]) + "\n"
 
         return text
 
