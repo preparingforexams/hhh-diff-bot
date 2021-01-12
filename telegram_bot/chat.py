ab3005278d83638b82a227a1a4c75e592a661dda
Torben Carstens <github@carstens.tech>
2021-01-12 19:11:08 +0100

    Persist invite link

--- telegram_bot/chat.py
+++ telegram_bot/chat.py
@@ -50,7 +50,8 @@ class Chat:
             "id": self.id,
             "pinned_message_id": self.pinned_message_id,
             "users": [user.serialize() for user in self.users],
-            "title": self.title
+            "title": self.title,
+            "invite-link": self.invite_link
         }
 
         return serialized
@@ -67,6 +68,7 @@ class Chat:
         chat.pinned_message_id = json_object.get("pinned_message_id")
         chat.users = {User.deserialize(user_json_object) for user_json_object in json_object.get("users", [])}
         chat.title = json_object.get("title", None)
+        chat.invite_link = json_object.get("invite-link", None)
 
         return chat
 
