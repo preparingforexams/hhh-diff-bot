81144b01bc3e07c966385cc9887243bde8b1cee3
Torben Carstens <git@carstens.tech>
2022-06-16 22:50:20 +0200

    Silence faulty inspection for statement

--- telegram_bot/decorators.py
+++ telegram_bot/decorators.py
@@ -117,6 +117,9 @@ class Command:
                     exception = PermissionError()
 
             if self.chat_admin:
+                # noinspection PyArgumentList
+                # this is for current_chat.administrators, pycharm believes that the `clz` parameter
+                # for the @group parameter is not present (which is wrong since `current_chat` is the clz parameter
                 if current_chat.type == chat.ChatType.PRIVATE:
                     log.debug("Execute function due to coming from a private chat")
                 elif current_user in current_chat.administrators():
