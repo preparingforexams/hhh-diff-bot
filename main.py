0aa03c4b48eebb9d2d86c63724b86012d7f1635d
Torben Carstens <git@carstens.tech>
2023-01-12 16:11:15 +0100

    Update main.py
    Co-authored-by: Björn Petersen <git@bjoernpetersen.net>

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from telegram.ext import CommandHandler, Updater, MessageHandler, Filters
 from telegram_bot import Bot, create_logger
 
 
-def update_state(state_filepath: str, *, state_mutation_function: Callable[[Dict], Dict] = None,
-                 chat_mutation_function: Callable[[Dict], Dict] = None):
+def update_state(state_filepath: str, *, state_mutation_function: Callable[[Dict], Dict] | None = None,
+                 chat_mutation_function: Callable[[Dict], Dict] | None = None):
     import json
     if not state_mutation_function:
         state_mutation_function = lambda x: x
