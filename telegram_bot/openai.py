31064ead21e3731de84c11dabd75de8c13c1ecbb
Torben Carstens <git@carstens.tech>
2023-03-08 11:50:25 +0100

    Handle OpenAI errors

--- telegram_bot/openai.py
+++ telegram_bot/openai.py
@@ -2,6 +2,7 @@ import inspect
 
 import openai as openai
 import requests
+from openai import InvalidRequestError, OpenAIError
 from requests import RequestException
 
 from .logger import create_logger
@@ -11,12 +12,23 @@ def generate_thumbnail(title: str):
     logger = create_logger(inspect.currentframe().f_code.co_name)
     logger.debug(f"generate thumbnail for {title}")
 
-    response = openai.Image.create(
-        prompt=title,
-        n=1,
-        size="512x512",
-        response_format="url",
-    )
+    try:
+        response = openai.Image.create(
+            prompt=title,
+            n=1,
+            size="512x512",
+            response_format="url",
+        )
+    except InvalidRequestError as e:
+        # Only ever saw this because of their profanity filter. Of course the error
+        # code was fucking None, so I would have to check the message to make sure
+        # that the error is actually about their "safety system", but I won't.
+        logger.debug("Got InvalidRequestError from OpenAI", exc_info=e)
+        return None
+    except OpenAIError as e:
+        logger.error("An error occurred during image generation", exc_info=e)
+
+        return None
     url = response["data"][0]["url"]
 
     try:
