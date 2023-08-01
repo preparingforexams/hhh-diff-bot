83be1cf1a58edea732321e307016effe4b1ac686
Torben Carstens <git@carstens.tech>
2023-08-01 16:26:58 +0200

    Create configmap if it doesn't exist

--- telegram_bot/state.py
+++ telegram_bot/state.py
@@ -3,6 +3,8 @@ import json
 import os
 from typing import Any, Optional
 
+import kubernetes.client
+from kubernetes import client
 from kubernetes.client import V1ConfigMap
 
 from .chat import Chat
@@ -37,7 +39,7 @@ class State:
 
 class ConfigmapState(State):
     def __init__(self, kubernetes_api_client, state: dict[str, Any]):
-        self.api = kubernetes_api_client
+        self.api: kubernetes.client.CoreV1Api = kubernetes_api_client
         self.name = os.getenv("CONFIGMAP_NAME")
         self.namespace = os.getenv("CONFIGMAP_NAMESPACE")
 
@@ -45,6 +47,26 @@ class ConfigmapState(State):
             raise ValueError("`CONFIGMAP_NAME` and `CONFIGMAP_NAMESPACE` have to be defined")
         self.configmap: Optional[V1ConfigMap] = None
         super().__init__(state)
+
+    def initialize(self, bot):
+        create = True
+        for configmap in self.api.list_namespaced_config_map(self.namespace):
+            if configmap.metadata.dictionary["name"] == self.name:
+                create = False
+                break
+
+        if create:
+            configmap = client.V1ConfigMap(
+                api_version="v1",
+                kind="ConfigMap",
+                metadata=client.V1ObjectMeta(
+                    name=self.name,
+                    namespace=self.namespace,
+                ),
+                data=self.state
+            )
+            self.api.create_namespaced_config_map(self.namespace, configmap)
+        super().initialize(bot)
 
     def read(self) -> dict[str, Any]:
         self.configmap = self.api.read_namespaced_config_map(self.name, self.namespace)
